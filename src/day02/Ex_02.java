8b86bcd4d30f3b9378dd04ddca3c2efff80a90b1
liked <liked@DESKTOP-S8SQP0N>
2018-09-11 16:17:16 +0900

    fir java programming

---
+++ src/day02/Ex_02.java
@@ -0,0 +1,18 @@
+package day02;
+
+public class Ex_02 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JJJJJJJJJJ     AAAAAAA     VV         VV     AAAAAAA");
+		System.out.println("JJJJJJJJJJ    AAAAAAAAA     V         V     AAAAAAAAA");
+		System.out.println("       JJJ   AAA     AAA    VV        VV   AAA     AAA");
+		System.out.println("       JJJ   AAA     AAA     V        V    AAA     AAA");
+		System.out.println("       JJJ   AAA     AAA      V      V     AAA     AAA");
+		System.out.println("J      JJJ   AAAAAAAAAAA       V    V      AAAAAAAAAAA");
+		System.out.println("JJ     JJJ   AAA     AAA        V  V       AAA     AAA");
+		System.out.println(" JJJJJJJJJ   AAA     AAA         VV        AAA     AAA");
+		System.out.println("  JJJJJJ     AAA     AAA         V         AAA     AAA");
+		System.out.println("   JJJ       AAA     AAA         V         AAA     AAA");
+		
+	}
+}
---
+++ src/day02/Ex_04.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class Ex_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("     *     ");
+			System.out.println("    ***    ");
+			System.out.println("   *****   ");
+			System.out.println("  *******  ");
+			System.out.println("   *****   ");
+			System.out.println("    ***    ");
+			System.out.println("     *     ");
+			
+	}
+
+}
